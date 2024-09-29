cc54c3bbcf8cbc16383c063994ea21c4ec31c4ab
KatePril <ket14pril14@gmail.com>
2024-09-29 22:21:02 +0300

    Update task 2

--- bin/async_tasks.dart
+++ bin/async_tasks.dart
@@ -1,20 +1,6 @@
 // import 'package:async_tasks/task1.dart' show demonstrateTaskOne;
-import 'package:async_tasks/task2.dart' show AsyncFutureMapper;
+import 'package:async_tasks/task2.dart' show demonstrateTaskTwo, demonstrateTaskTwoV2;
 
 void main() async {
-  final asyncFutureMapper = AsyncFutureMapper([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-  asyncFutureMapper.asyncMap((x) => x * x).then((list) async {
-    for (final element in list) {
-      await Future.delayed(Duration(seconds: 1));
-      print("1: $element");
-    }
-  });
-
-  asyncFutureMapper.asyncFilter((x) => x % 2 == 1).then((list) async {
-    for (final element in list) {
-      await Future.delayed(Duration(seconds: 1));
-      print("2: $element");
-    }
-  });
+  demonstrateTaskTwoV2();
 }
--- lib/task2.dart
+++ lib/task2.dart
@@ -25,3 +25,34 @@ class AsyncFutureMapper<T, E> {
     return res;
   }
 }
+
+void demonstrateTaskTwo() {
+  final asyncFutureMapper = AsyncFutureMapper([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  asyncFutureMapper.asyncMap((x) => x * x).then((list) async {
+    for (final element in list) {
+      await Future.delayed(Duration(seconds: 1));
+      print("1: $element");
+    }
+  });
+
+  asyncFutureMapper.asyncFilter((x) => x % 2 == 1).then((list) async {
+    for (final element in list) {
+      await Future.delayed(Duration(seconds: 1));
+      print("2: $element");
+    }
+  });
+}
+
+void demonstrateTaskTwoV2() async {
+  final asyncFutureMapper = AsyncFutureMapper([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  final list = await asyncFutureMapper.asyncMap((x) => x * x);
+
+  final asyncFutureMapperTwo = AsyncFutureMapper(list);
+  final filteredList = await asyncFutureMapperTwo.asyncFilter((x) => x % 2 == 0);
+
+  for (var element in filteredList) {
+    await Future.delayed(Duration(seconds: 1));
+      print(element);
+  }
+}
