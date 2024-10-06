d80b9ac27dcfa22036d2c8b2aa27331a1f657694
KatePril <ket14pril14@gmail.com>
2024-10-06 18:07:23 +0300

    Update task 3

--- bin/async_tasks.dart
+++ bin/async_tasks.dart
@@ -1,9 +1,11 @@
 
 // import 'package:async_tasks/task1/ready/task1_ready.dart' show testAsyncMap;
 import 'package:async_tasks/task3/test_cancellable.dart' show testCancellable;
+import 'package:async_tasks/task3/task3.dart' show demoCancellableFuture;
 
 void main() async {
   // testCancellable(2);
-  testCancellable([1, 2, 3, 4, 5, 6]);
+  // testCancellable([1, 2, 3, 4, 5, 6]);
+  demoCancellableFuture();
 }
 
---
+++ lib/task3/task3.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'package:async/async.dart';
+
+final cancellableOperation = CancelableOperation.fromFuture(Future.value(0));
+
+Future<dynamic> asyncMap(el, func) async => Future.delayed(Duration(seconds: 1), () => func(el)).catchError((err) {
+  print(err);
+  return cancellableOperation.cancel();
+});
+
+void demoCancellableFuture() async {
+  List<dynamic> l = [4, 5, 6, 1, "2", 3, "7", 8, 9];
+  List<Future<dynamic>> fl = l.map((el) => asyncMap(el, (x) {
+    final r = Future.value(x * x).catchError((e) => cancellableOperation.cancel());
+    print(x);
+    return r;
+  })).toList();
+
+  List<dynamic> res = await Future.wait(fl);
+  print(res);
+}
