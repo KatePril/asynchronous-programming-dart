6af6e2d327c3bce555834f2f01b3afe2e700b829
KatePril <ket14pril14@gmail.com>
2024-10-06 18:25:12 +0300

    Add onError

--- lib/task3/task3.dart
+++ lib/task3/task3.dart
@@ -3,15 +3,18 @@ import 'package:async/async.dart';
 
 final cancellableOperation = CancelableOperation.fromFuture(Future.value(0));
 
-Future<dynamic> asyncMap(el, func) async => Future.delayed(Duration(seconds: 1), () => func(el)).catchError((err) {
-  print(err);
-  return cancellableOperation.cancel();
-});
+Future<dynamic> asyncMap(el, func) async => Future.delayed(Duration(seconds: 1), () => func(el)).catchError((err) => onError(err));
+
+int onError(e) {
+  print(e);
+  cancellableOperation.cancel();
+  return 0;
+}
 
 void demoCancellableFuture() async {
   List<dynamic> l = [4, 5, 6, 1, "2", 3, "7", 8, 9];
   List<Future<dynamic>> fl = l.map((el) => asyncMap(el, (x) {
-    final r = Future.value(x * x).catchError((e) => cancellableOperation.cancel());
+    final r = Future.value(x * x).catchError((e) => onError(e));
     print(x);
     return r;
   })).toList();
