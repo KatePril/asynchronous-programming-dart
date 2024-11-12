99323c603c583e8e855c18edc5a7fe7260936f74
KatePril <ket14pril14@gmail.com>
2024-11-12 14:49:08 +0200

    Add error handling to task 5-1

--- lib/task5/task5-1.dart
+++ lib/task5/task5-1.dart
@@ -3,20 +3,42 @@ import 'package:events_emitter/events_emitter.dart';
 void demoEventEmitter() {
   EventEmitter emitter = EventEmitter();
 
-  emitter.on('message', (String data) => print('String: $data'));
-  emitter.on('message', (int data) => print('Integer: $data'));
+  emitter.on('message', (String data) {
+    try {
+      if (data.isNotEmpty) {
+        print('String: $data');
+      } else {
+        throw Exception("The string is empty");
+      }
+    } catch(error) {
+      emitter.emit('error', error);
+    }
+  });
+  emitter.on('message', (int data) {
+    try {
+      if (data != 0) {
+        print('Integer: ${100 / data}');
+      } else {
+        throw Exception("Division by zero");
+      }
+    } catch(error) {
+      emitter.emit('error', error);
+    }
+  });
   emitter.on('error', (error) => print('Caught error: $error'));
 
   emitter.once('message', (double data) => print('Double: $data'));
 
   emitter.emit('message', 'Hi there');
-  emitter.emit('message', 'Orange');
+  emitter.emit('message', '');
   emitter.emit('message', 42);
-  emitter.emit('message', 52);
+  emitter.emit('message', 0);
   emitter.emit('message', 4.2);
   emitter.emit('message', 1.2);
 
   emitter.emit('error', Error());
+
+  emitter.emit('someEvent', "Hello world");
 
   emitter.listeners.clear();
 
